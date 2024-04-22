56439b11b3748fe672a951197a3ceba33c0aa315
phillip <pdang2@uw.edu>
2024-04-21 23:54:35 -0700

    finished hw

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -13,7 +13,78 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var playerOne: UILabel!
+    @IBOutlet weak var playerTwo: UILabel!
+    
+    @IBOutlet weak var GameOverOne: UILabel!
+    @IBOutlet weak var GameOverTwo: UILabel!
+    
+    var playerOneLife = 20
+    var playerTwoLife = 20
+    
+    @IBAction func btn1(_ sender: UIButton) {
+        print("test")
+        playerOneLife += 1
+        playerOne.text = "Player 1: " + String(playerOneLife)
+        if (playerOneLife <= 0) {
+            GameOverOne.isHidden = false
+        }
+    }
+    
+    @IBAction func btn2(_ sender: UIButton) {
+        playerOneLife -= 1
+        playerOne.text = "Player 1: " + String(playerOneLife)
+        if (playerOneLife <= 0) {
+            GameOverOne.isHidden = false
+        }
+    }
+    
+    @IBAction func btn3(_ sender: UIButton) {
+        playerOneLife += 5
+        playerOne.text = "Player 1: " + String(playerOneLife)
+        if (playerOneLife <= 0) {
+            GameOverOne.isHidden = false
+        }
+    }
+    
+    @IBAction func btn4(_ sender: UIButton) {
+        playerOneLife -= 5
+        playerOne.text = "Player 1: " + String(playerOneLife)
+        if (playerOneLife <= 0) {
+            GameOverOne.isHidden = false
+        }
+    }
+    
+    @IBAction func btn5(_ sender: UIButton) {
+        playerTwoLife += 1
+        playerTwo.text = "Player 2: " + String(playerTwoLife)
+        if (playerTwoLife <= 0) {
+            GameOverTwo.isHidden = false
+        }
+    }
+    
+    @IBAction func btn6(_ sender: UIButton) {
+        playerTwoLife -= 1
+        playerTwo.text = "Player 2: " + String(playerTwoLife)
+        if (playerTwoLife <= 0) {
+            GameOverTwo.isHidden = false
+        }
+    }
+    
+    @IBAction func btn7(_ sender: UIButton) {
+        playerTwoLife += 5
+        playerTwo.text = "Player 2: " + String(playerTwoLife)
+        if (playerTwoLife <= 0) {
+            GameOverTwo.isHidden = false
+        }
+    }
+    
+    @IBAction func btn8(_ sender: UIButton) {
+        playerTwoLife -= 5
+        playerTwo.text = "Player 2: " + String(playerTwoLife)
+        if (playerTwoLife <= 0) {
+            GameOverTwo.isHidden = false
+        }
+    }
 }
-
